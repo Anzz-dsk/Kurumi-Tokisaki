ec60d84944cbe03a221f188f219ee6e7d322c57f
Anzz dsk <daisukexzz@gmail.com>
2025-06-15 10:02:33 -0400

    〆 Anzz ⍻ Dsk ᜫ

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1 align="center">ㄨ Kurumi〆Tokisaki ㄨ</p>
+<p>
+        <img src= "https://qu.ax/CfbhR.jpg">
+    </p>
+    <p align="center">
+        <a href="#"><img title="simple-whatsapp-bot" src="https://img.shields.io/badge/-SIMPLE--WHATSAPP--BOT-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+    </p>
+    <p>
---
+++ settings.js
@@ -0,0 +1,66 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+  ['50238999796', 'Irokz Dal ダーク', true],
+  ['584162967777', 'Hans', true]
+]
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.prems = []
+   
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = ``
+global.author = '〆𝑲𝒖𝒓𝒖𝒎𝒊⍻𝑻𝒐𝒌𝒊𝒔𝒂𝒌𝒊 ⸙\n\n𝑪𝒓𝒆𝒂𝒅𝒂 𝑷𝒐𝒓 𝑨𝒏𝒛𝒛ㄨ𝑫𝒔𝒌'
+global.wait = '𝑨𝒈𝒖𝒂𝒓𝒅𝒆 𝑼𝒏 𝑴𝒐𝒎𝒆𝒏𝒕𝒐...'
+global.botname = '𝙠𝙪𝙧𝙪𝙢𝙞 ⸙ 𝙏𝙤𝙠𝙞𝙨𝙖𝙠𝙞'
+global.textbot = `Powered by Anzz Dsk`
+global.listo = '*𝑨𝒒𝒖𝒊 𝑻𝒊𝒆𝒏𝒆 ฅ^•ﻌ•^ฅ*'
+global.namechannel = '〆 Anzz - Dsk - Kurumi 〆'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
+global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.group = 'https://chat.whatsapp.com/HrnFl3gBPMNEn2AUDrtsyK'
+global.group2 = 'https://chat.whatsapp.com/HrnFl3gBPMNEn2AUDrtsyK'
+global.group3 = 'https://chat.whatsapp.com/HrnFl3gBPMNEn2AUDrtsyK'
+global.canal = 'https://whatsapp.com/channel/0029VbAKCwH9Gv7QYlxCKw0O'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "50238999796-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: botname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.multiplier = 69 
+global.maxwarn = '2' // máxima advertencias
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
